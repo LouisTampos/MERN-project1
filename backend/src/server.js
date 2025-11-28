97c6f891dc16ab73b99ec643e0b9e56a1d3bfb9e
Louis Tampos <tamposvictorlouis@gmail.com>
2025-11-29 07:09:30 +0800

    added mongoDB

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
+import {  connectDB } from "./config/db.js"
+
+const app = express();
+
+connectDB();
+
+app.use("/api/myNotes", notesRoutes);
+
+app.listen(5001, () => {
+   console.log("Server started on PORT: 5001");
+});
