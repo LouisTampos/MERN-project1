f1e9ff59cf78497099627851d3f93c2ede0a945e
Louis Tampos <tamposvictorlouis@gmail.com>
2025-11-30 21:47:20 +0800

    middleware,controllers,server additionals routes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-      await mongoose.connect("mongodb+srv://tamposvictorlouis_db_user:dqOWxA8yFKPsxxfb@cluster0.jtybuvt.mongodb.net/?appName=Cluster0")
+      await mongoose.connect(process.env.MONGO_URI)
       
       console.log("MONGODB CONNECTED SUCCESSFULLY!")
     } catch (error) {
       console.error("Error connecting to MONGODB", error);
+      process.exit(1); //exit with failure
     }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import {  connectDB } from "./config/db.js"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001
 
 connectDB();
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+   next();
+})
 
 app.use("/api/myNotes", notesRoutes);
 
 app.listen(5001, () => {
-   console.log("Server started on PORT: 5001");
+   console.log("Server started on PORT:", PORT);
 });
