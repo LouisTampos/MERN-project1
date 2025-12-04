f524a60f78653f83a54ec0211001d3a5e14375d7
Louis Tampos <tamposvictorlouis@gmail.com>
2025-12-04 12:51:41 +0800

    utils/ formatdate/notecard fixed from server

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 5001
 
 app.use(cors({
-   origin: "http://localhost:5173" 
+   origin: "http://localhost:5173", 
+   credentials:true
  })
 )
 app.use(express.json())
